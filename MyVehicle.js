107484d16b57231f52cc3975fa9c94b667430ac0
davide-castro20 <davideafc@gmail.com>
2020-04-14 22:02:31 +0100

    Fixed vehicle position coordenates

--- MyVehicle.js
+++ MyVehicle.js
@@ -8,6 +8,7 @@ class MyVehicle extends CGFobject {
         this.mesh = new MyPyramid(scene, 3, 1);
         this.posX = 0.0;
         this.posY = 0.0;
+        this.posZ = 0.0;
     }
 
     initBuffers() {
@@ -17,6 +18,7 @@ class MyVehicle extends CGFobject {
     display() {
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.translate(this.posX, this.posZ, -this.posY);
         this.scene.translate(0, -0.5, 0);
         this.mesh.display();
         this.scene.popMatrix();
